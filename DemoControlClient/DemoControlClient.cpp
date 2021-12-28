87a052f60cebfe7a8ec56af98a9cc98d143e02b3
SodaCris <18463922396@163.com>
2021-12-28 14:05:39 +0800

    add demo client

---
+++ DemoControlClient/DemoControlClient.cpp
@@ -0,0 +1,49 @@
+﻿#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <boost/asio.hpp>
+#include <sstream>
+
+using boost::asio::ip::tcp;
+
+enum { max_length = 1024 };
+
+
+int main()
+{
+    boost::asio::io_context io_context;
+
+    tcp::resolver resolver(io_context);
+    tcp::resolver::results_type endpoints =
+        resolver.resolve(tcp::v4(), "127.0.0.1", "1236");
+
+    tcp::socket s(io_context);
+    
+
+    std::stringstream ss;
+    std::string input;
+    while (true) {
+        try {
+            boost::asio::connect(s, endpoints);
+            break;
+        }
+        catch(std::exception ex) {
+            continue;
+        }
+    }
+    std::cout << "connected. \n";
+    while (true) {
+        std::cout << "input: ";
+        std::cin >> input;
+        ss.clear();
+
+        ss << input;
+        std::string message = ss.str();
+        boost::asio::write(s, boost::asio::buffer(input, input.length()));
+        if (input == "exit") {
+            break;
+        }
+    }
+    return 0;
+}
+
---
+++ TestBoostDemo/TestBoostDemo.cpp
@@ -0,0 +1,10 @@
+﻿
+#include <boost/process.hpp>
+#include <boost/process/start_dir.hpp>
+
+int main() {
+	namespace bp = boost::process;
+
+	int result = bp::system("D:/src/TaiYuan/py/core/python.exe", "copy_file.py", bp::start_dir("D:/src/TaiYuan/py/"));
+
+}
